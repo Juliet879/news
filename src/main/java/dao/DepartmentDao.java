e363e9a9f26237ffc9c78e9423046a4cebb0f23b
JulietK <julietgisemba000@>
2020-09-30 09:31:26 +0300

    modified files

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -3,21 +3,7 @@ import models.*;
 import java.util.List;
 
 public interface DepartmentDao {
-
-    //create
+// create
     void add(Department department);
-    void addDepartmentToUser(Department department, User user);
-
-    //read
-    List<Department> getAll();
-    Department findById(int id);
-    List<User> getAllUsersByDepartment(int departmentId);
-
-    //update
-    void update(int id, String departmentName, String departmentDescription, int departmentEmployeesNumber);
-
-    //delete
-    void deleteById(int id);
-    void clearAll();
 
 }
