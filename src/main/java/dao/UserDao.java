1e2e6c2a538af49369df715569f367d6d5d1557b
JulietK <julietgisemba000@>
2020-09-30 06:12:58 +0300

    modified files

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,22 @@
 package dao;
+import models.*;
+import java.util.List;
 
 public interface UserDao {
+
+    //create
+    void add (User user);
+
+    //read
+    List<User> getAllUsers();
+    User findUserById(int id);
+    List<User> getAllUsersByDepartment(int departmentId);
+
+    //update
+    void update(int id, String userName,  String userCompanyPosition, String useCompanyRole, int departmentId);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
 }
